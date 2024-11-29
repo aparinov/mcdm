aeed10a0c3e174e5672f47a4360a94af96826f7d
aparinov <aparinov@gmail.com>
2024-11-29 15:22:44 +0300

    Add files via upload

---
+++ MCDM.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="MCDM", page_icon=":bar_chart:")
+st.header("MCDM")
+st.markdown(' ##### Multi-Criteria Decision Making ')
+st.write("It is a process used to make decisions when there are multiple conflicting criteria that must be considered. MCDM methods use mathematical models to evaluate the different options and their potential outcomes, considering various factors.")
+
+st.write("---")
+st.write("##")
+
+st.markdown(" ##### MCDM Techniques")
+st.write(" - Analytic Hierarchy Process (AHP)")
+st.write(" - Technique for Order Preference by Similarity to Ideal Solution (TOPSIS)")
+st.write(" - Elimination and Choice Expressing Reality (ELECTRE)")
+
+
+st.write("---")
+st.write("##")
+
+st.markdown(" ##### Applications of MCDM")
+st.write(" - Business decision making")
+st.write(" - Engineering design")
+st.write(" - Environmental management")
+st.write(" - Medical decision making")
+
+st.write("---")
+st.write("##")
+
